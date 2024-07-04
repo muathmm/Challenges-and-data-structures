cfbcc46464df69f4a4d0594765dddf668d3cb538
Muath Mhawich <23041194@student.ltuc.com>
2024-07-04 04:45:10 +0300

    "solve CC-6-B"

---
+++ Challenges/Reverse-Words/README.md
@@ -0,0 +1,26 @@
+# challenges-and-data-structures
+
+## Table of Contents
+
+### Whiteboard Challenges
+
+#### Challenge A: Array Reversal
+[Array Reversal Whiteboard](whiteboard-challenges/challenge-A.jpg)
+
+#### Challenge B: Most Frequent Number
+[Most Frequent Number Whiteboard](whiteboard-challenges/challenge-B.jpg)
+
+### Challenge 02: Maximum Value
+[Maximum Value Whiteboard](challenge2/max-value.jpg)
+### Challenge 03: Remove-Middle-Value
+
+[Remove Middle Value from Array  Whiteboard](whiteboard-challenges/remove-middle.jpg)
+
+### Challenge 04: insert-Middle-Value
+
+[insert Middle Value from Array  Whiteboard](whiteboard-challenges/Insert-Middle.jpg)
+### Challenge 05: Finding Duplicate Elements in an Array
+[Finding Duplicate Elements in an Array](whiteboard-challenges/FindDuplicate.jpg)
+
+### Challenge 06: Finding Common Elements in two arrays
+[Finding common Elements ](whiteboard-challenges/CommonElements.jpg)
---
+++ Challenges/Reverse-Words/ReverseWords/Reverse-String.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords
+{
+    public class Reverse_String
+    {
+
+        public static string ReverseWords(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+
+            List<string> words = new List<string>();
+            int start = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                {
+                    words.Add(input.Substring(start, i - start));
+                    start = i + 1;
+                }
+            }
+           
+            words.Add(input.Substring(start));
+
+         
+            for (int i = 0, j = words.Count - 1; i < j; i++, j--)
+            {
+                string temp = words[i];
+                words[i] = words[j];
+                words[j] = temp;
+            }
+
+           
+            string result = string.Empty;
+            for (int i = 0; i < words.Count; i++)
+            {
+                result += words[i];
+                if (i < words.Count - 1)
+                {
+                    result += " ";
+                }
+            }
+
+            return result;
+        }
+    }
+}
